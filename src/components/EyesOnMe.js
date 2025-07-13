7d627dd7ad1d9b5c9283af471e3113eb5fcef6ad
zakarie <zakarie.abdi@student.moringaschool.com>
2025-07-13 18:32:57 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,29 @@
 // Code EyesOnMe Component Here
+ import React from "react";
+
+ function EyesOnMe(){
+    
+    function focusEvent(){
+        console.log("Good")
+    }
+
+    function blurEvent (){
+        console.log("Hey! Eyes on me")
+    }
+
+    return(
+        <div>
+            <button
+            
+            onFocus={focusEvent}
+            onBlur={blurEvent}
+            >
+                Eyes on me
+            </button>
+
+                
+            
+        </div>
+    )
+}
+ export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,26 @@
 // Code Keypad Component Here
 
+import React from "react";
+
 function Keypad (){
+
+    function keypadHandler(e){
+        console.log(e.target.value)
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <input 
+            
+            type="password"
+            // value={}
+            onChange={keypadHandler}
+            placeholder="Enter Your Password"
+
+
+            />
+        </div>
     )
 }
 
